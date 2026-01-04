8b50dabf92cfc51547a361643f8ff14e436e19fe
Erhan Güney Karadağ <40764896+ErhanGuneyKaradag@users.noreply.github.com>
2026-01-04 19:37:11 +0300

    Add files via upload
    First Commit

---
+++ mod.api_module.php
@@ -0,0 +1,12 @@
+<?php
+
+class Api_module
+{
+    public $return_data = '';
+
+    public function __construct()
+    {
+        // Module constructor - genelde tag'ler için kullanılır
+        // API routing Extension'da yapılıyor
+    }
+}
